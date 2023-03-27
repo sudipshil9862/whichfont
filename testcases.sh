506ef4260c3890d7a237dc4074dec52a947c486e
sudipshil9862 <sudipshil9862@gmail.com>
2023-03-27 16:07:23 +0530

    run testcases.sh to avoid running multiple commands to debug
    just run ./testcases.sh
    find results in test_results.txt
    use gedit or vim. for better view use gedit

---
+++ testcases.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+rm test_results.txt
+meson builddir
+ninja -C builddir
+
+test_cases=(
+    "u+0985"
+    "U+0985"
+    "U+0985sudiphex"
+    "U+sudip"
+    "0x0985"
+    "0X0985"
+    "0x0985sudiphex"
+    "0xsudip"
+    "0985"
+    "-a 0985"
+    "-s 0985"
+    "😀"
+    "-a 😀"
+    "-s 😀"
+    "অah😀"
+    "-a অah😀"
+    "-s অah😀"
+    "A"
+    "-a A"
+    "-s A"
+)
+
+output_file="test_results.txt"
+
+for test_case in "${test_cases[@]}"
+do
+    command_output=$(./builddir/matchFontForUTF8 $test_case)
+    echo "input: ./builddir/matchFontForUTF8 $test_case" >> $output_file
+    echo "output: $command_output" >> $output_file
+    echo "-------------------------------------------------------------------------------------------------------------------------------------" >> $output_file
+done
